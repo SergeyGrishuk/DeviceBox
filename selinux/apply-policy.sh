1e0a99c16e3add92434c5332406d8fe8db51a766
SergeyGrishuk <sergeygrishuk1995@gmail.com>
2025-12-16 08:51:51 +0200

    Update script
    Add a check for checkmodule command

--- selinux/apply-policy.sh
+++ selinux/apply-policy.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 
+set -e
+
+
 # This script complies and applies the SELinux policy. See the Wiki for more details.
+
+if [[ -z "$(which checkmodule)" ]]
+then
+    echo -e '[-] Please install the `checkpolicy` and `policycoreutils-python-utils` packages.\nRun the following command and then rerun the script:\ndnf install -y checkpolicy policycoreutils-python-utils'
+
+    exit
+fi
 
 checkmodule -M -m -o devicebox_heap.mod devicebox_heap.te
 semodule_package -o devicebox_heap.pp -m devicebox_heap.mod
