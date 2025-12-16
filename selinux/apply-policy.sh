0c1fe9e5412548cdba4be53d885502df9d381c10
SergeyGrishuk <sergeygrishuk1995@gmail.com>
2025-12-16 17:11:14 +0200

    Add echo command

--- selinux/apply-policy.sh
+++ selinux/apply-policy.sh
@@ -16,3 +16,5 @@ fi
 checkmodule -M -m -o devicebox_heap.mod devicebox_heap.te
 semodule_package -o devicebox_heap.pp -m devicebox_heap.mod
 sudo semodule -i devicebox_heap.pp
+
+echo "[+] Policy applied successfully"
