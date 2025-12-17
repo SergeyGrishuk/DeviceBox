aa467982e4608ef6053b42ffb7f50dca41cc6ab2
SergeyGrishuk <sergeygrishuk1995@gmail.com>
2025-12-17 15:43:08 +0200

    Add ANDROID_VERSION and ANDROID_API_LEVEL variables

--- build-container.sh
+++ build-container.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 
 
-docker build . -f Dockerfiles/Dockerfile -t devicebox:latest
+
+ANDROID_VERSION=${1}
+ANDROID_API_LEVEL=${2}
+
+
+if [[ -z ${ANDROID_VERSION} ]]
+then
+    echo "[-] Please specify Android version"
+
+    exit 1
+fi
+
+if [[ -z "${ANDROID_API_LEVEL}" ]]
+then
+    echo "[-] Please specify Android API level"
+
+    exit 1
+fi
+
+docker build . \
+    --build-arg ANDROID_VERSION= \
+    --build-arg ANDROID_API_LEVEL= \
+    -f Dockerfiles/Dockerfile \
+    -t devicebox:latest
