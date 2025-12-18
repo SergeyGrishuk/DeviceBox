3d343f273dbac101af8e7fa062173d50c45fbe84
SergeyGrishuk <sergeygrishuk1995@gmail.com>
2025-12-18 07:58:12 +0200

    Add DEVICE_NAME variable

--- scripts/start-device.sh
+++ scripts/start-device.sh
@@ -12,12 +12,15 @@ then
 fi
 
 
+DEVICE_NAME="$(echo -n "DeviceBox_${DEVICE}" | tr ' ' '_')"
+
+
 # Create a new test device
 echo "no" | avdmanager create avd \
-    --name devicebox_device \
+    --name "${DEVICE_NAME}" \
     --package "system-images;android-${ANDROID_API_LEVEL};google_apis;x86_64" \
     --device "${DEVICE}"
 
 
 # Run the test device
-emulator -avd devicebox_device -no-audio -no-boot-anim -gpu swiftshader_indirect -no-metrics
+emulator -avd "${DEVICE_NAME}" -no-audio -no-boot-anim -gpu swiftshader_indirect -no-metrics
